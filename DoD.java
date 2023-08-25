5aecd04efa9fc9aab7239337f77b341398299e89
Samekah <lucidareverie@outlook.com>
2023-08-25 12:42:32 +0100

    updated intro message

--- DoD.java
+++ DoD.java
@@ -1,14 +1,15 @@
+// import java.util.Arrays;
+
 public class DoD {
     public static void main(String[] args) {
 		GameLogic gl = new GameLogic();
-				
-		//playerStartPosition(); doesn't work :(
 		
 		System.out.println("Welcome to dugeons of doom! The following commands are available to you:");
-		System.out.println("LOOK, HELLO, PICKUP, QUIT, MOVE N, MOVE E, MOVE S, MOVE W");
+		System.out.println("LOOK, HELLO, PICKUP, MOVE N, MOVE E, MOVE S, MOVE W, LOAD, and QUIT");
+		System.out.printf("\nCurrently the default map \"%s\" is loaded.\n", gl.map.getMapName());
 		
 		while(gl.gameRunning()) {
-			System.out.print(gl.hp.getInputFromConsole() + "\n");
+			System.out.print(gl.hp.getInputFromConsole() + "\n\n");
 		}
 		
 		try {
